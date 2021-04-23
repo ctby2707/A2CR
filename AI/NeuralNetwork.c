8067ce51b2f28b5a63c03a2846ad7430bf588871
Alexandre Bourcier <alexandre.bourcier@epita.fr>
2021-04-23 17:08:13 +0200

    modulable AI

--- AI/NeuralNetwork.c
+++ AI/NeuralNetwork.c
@@ -12,82 +12,173 @@
 
 int main()
 {
-  double* input1 = malloc(NB_INPUT * sizeof(double));
-  double* input2 = malloc(NB_NEURON_LAYER_1 * sizeof(double));
-  double* input3 = malloc(NB_NEURON_LAYER_2 * sizeof(double));
+  int nb_layer = 3;
+  int *nb_neuron = malloc(nb_layer*sizeof(int));
+  nb_neuron[0] = NB_INPUT;
+  nb_neuron[1] = NB_NEURON_LAYER_1;
+  nb_neuron[3] = NB_NEURON_LAYER_2;
 
-  double* weights1 = malloc(NB_INPUT * NB_NEURON_LAYER_1 * sizeof(double));
-  double* weights2 = malloc(NB_NEURON_LAYER_1 * NB_NEURON_LAYER_2 * sizeof(double));
-  double* biasWeights = malloc((NB_NEURON_LAYER_1 + NB_NEURON_LAYER_2) * sizeof(double));
-
-  struct Neuron *neuronlist1 = malloc(NB_NEURON_LAYER_1 * sizeof(struct Neuron));
-  struct Neuron *neuronlist2 = malloc(NB_NEURON_LAYER_2 * sizeof(struct Neuron));
-
-  randomizeNetwork(weights1, NB_INPUT * NB_NEURON_LAYER_1);
-  randomizeNetwork(weights2, NB_NEURON_LAYER_1 * NB_NEURON_LAYER_2);
-  randomizeNetwork(biasWeights, NB_NEURON_LAYER_1 + NB_NEURON_LAYER_2);
-
-  //initialize hidden layer
-  for (int i = 0; i < NB_NEURON_LAYER_1; i++)
-  {
-    struct Neuron trans = neuronlist1[i];
-    trans.size = NB_INPUT;
-    trans.input = input1;
-    trans.weight = weights1 + i*NB_INPUT;
-    trans.biasWeight = biasWeights + i;
-    neuronlist1[i] = trans;
-  }
-
-  //initialize output layer
-  for (int i = 0; i < NB_NEURON_LAYER_2; i++)
-  {
-    struct Neuron trans = neuronlist2[i];
-    trans.size = NB_NEURON_LAYER_1;
-    trans.input = input2;
-    trans.weight = weights2 + i*NB_NEURON_LAYER_1;
-    trans.biasWeight = biasWeights + i+NB_NEURON_LAYER_1;
-    neuronlist2[i] = trans;
-  }
-
-  /* training function
-  int epoc = 0
-  while(epoc < 800)
-  {
-    for (int i = 0; i < 6; i++)
-    {
-      input2[i] = output(neuronlist1[i]);
-      adjust10Weight(neuronlist1[i]);
-    }
-    for (int i = 0; i < 4; i++)
-    {
-      input3[i] = output(neuronlist2[i]);
-      adjust10Weight(neuronlist2[i]);
-    }
-
-    epoc++;
-  }*/
-
-  savefile("NeuralData/save_biaisweight.txt", biasWeights, NB_NEURON_LAYER_1 + NB_NEURON_LAYER_2);
-  savefile("NeuralData/save_weight1.txt", weights1, NB_INPUT * NB_NEURON_LAYER_1);
-  savefile("NeuralData/save_weight2.txt", weights2, NB_NEURON_LAYER_1 * NB_NEURON_LAYER_2);
-
-  free(input1);
-  free(input2);
-  free(input3);
-  free(weights1);
-  free(weights2);
-  free(biasWeights);
-  free(neuronlist1);
-  free(neuronlist2);
+  double input[] = {0.95678,
+                    0.87654,
+                    0.95678,
+                    0.97654,
+                    0.96543,
+                    0.76546,
+                    0.96543,
+                    0.98765,
+                    0.93456,
+                    0.23456,
+                    0.96543,
+                    0.87654};
+  struct Network *network = init(nb_layer, nb_neuron);
+  randomizeNetwork(network);
+  printf("%c\n",execute_network(network, input));
+  save_Network(network);
+  destroy_network(network);
 }
 
-//define random valuer between -1 and 1 on tab
-void randomizeNetwork(double *tab, size_t size)
+struct Network *init(int nb_layer, int *nb_neuron)
 {
-  for(size_t i = 0; i < size; i++)
+  struct Network *network = malloc(sizeof(struct Network));
+  network->nb_layer = nb_layer;
+  network->nb_neuron_layer = nb_neuron;
+
+  int nb_total_neuron = 0;
+  int size_weight_list = 0;
+
+  int *nb_weights = malloc((nb_layer-1)*sizeof(int));
+  network->nb_weights = nb_weights; //list of nb weight for each layers
+
+  for(size_t i = 0; i < nb_layer; i++)
   {
-    double range = (2);
-    double div = RAND_MAX / range;
-    tab[i] = -1 + (rand() / div);
+    nb_total_neuron = nb_total_neuron + nb_neuron[i];
+    if(i != 0)
+    {
+      size_weight_list = size_weight_list + (nb_neuron[i-1] * nb_neuron[i]);
+      network->nb_weights[i] = (nb_neuron[i-1] * nb_neuron[i]);
+    }
   }
+  double *input = malloc(nb_total_neuron*sizeof(double));
+  network->input = input;
+
+  double *weights = malloc(size_weight_list*sizeof(double));
+  network->weights = weights;
+
+  double *biasWeights = malloc((nb_total_neuron - nb_neuron[0])*sizeof(double));
+  network->biasWeights = biasWeights;
+
+  struct Neuron *neuron_list = malloc((nb_total_neuron - nb_neuron[0])*sizeof(struct Neuron));
+  network->neuron_list = neuron_list;
+
+  int bias = 0;
+  for(size_t i = 1; i < nb_layer; i++)
+  {
+    for(size_t j = 0; j < nb_neuron[i]; j++)
+    {
+      network->neuron_list[j+bias].size = nb_neuron[i-1];
+      network->neuron_list[j+bias].input = network->input + bias;
+      network->neuron_list[j+bias].weight = network->weights + j*nb_neuron[i-1] + bias;
+      network->neuron_list[j+bias].biasWeight = network->biasWeights + j + bias;
+    }
+    bias = bias + nb_neuron[i];
+  }
+  return network;
+}
+char execute_network(struct Network *network, double *inputs)
+{
+  //enter the input
+  for(int i = 0; i < network->nb_neuron_layer[0]; i++)
+  {
+    network->input[i] = inputs[i];
+  }
+
+  //execute the network
+  int bias = 0;
+  for(size_t i = 1; i < network->nb_layer; i++)
+  {
+    for(size_t j = 0; j < network->nb_neuron_layer[i]; j++)
+    {
+      network->input[j+bias+network->nb_neuron_layer[0]] = output(network->neuron_list[j+bias]);
+    }
+    bias = bias + network->nb_neuron_layer[i];
+  }
+
+  int nb_total_neuron = 0;
+  for(size_t i = 0; i < (network->nb_layer-1); i++)
+  {
+    nb_total_neuron = nb_total_neuron + network->nb_neuron_layer[i];
+  }
+  int index = 0;
+  if(network->input[nb_total_neuron+index] < network->input[nb_total_neuron+1])
+    index = 1;
+  if(network->input[nb_total_neuron+index] < network->input[nb_total_neuron+2])
+    index = 2;
+  if(network->input[nb_total_neuron+index] < network->input[nb_total_neuron+3])
+    index = 3;
+
+  if(index == 0)
+    return 'N';
+  if(index == 1)
+    return 'S';
+  if(index == 2)
+    return 'G';
+  return 'D';
+}
+void destroy_network(struct Network *network)
+{
+  free(network->nb_neuron_layer);
+  free(network->neuron_list);
+  free(network->input);
+  free(network->nb_weights);
+  free(network->weights);
+  free(network->biasWeights);
+  free(network);
+}
+
+//define random valuer between -1 and 1 on Network
+void randomizeNetwork(struct Network *network)
+{
+  int size_weight_list = 0;
+  for(size_t i = 1; i < network->nb_layer; i++)
+  {
+    size_weight_list = size_weight_list + (network->nb_neuron_layer[i-1] * network->nb_neuron_layer[i]);
+  }//calculate the size of the weight list
+
+  for(size_t i = 0; i < size_weight_list; i++)
+  {
+    double range = 2;
+    double div = RAND_MAX / range;
+    network->weights[i] = -1 + (rand() / div);
+  }
+  int nb_total_neuron = 0;
+  for(size_t i = 1; i < network->nb_layer; i++)
+  {
+    nb_total_neuron = nb_total_neuron + network->nb_neuron_layer[i];
+  }//calculate the size of the biasWeight list
+
+  for(size_t i = 0; i < nb_total_neuron; i++)
+  {
+    double range = 2;
+    double div = RAND_MAX / range;
+    network->biasWeights[i] = -1 + (rand() / div);
+  }
+}
+
+void save_Network(struct Network *network)
+{
+  int nb_total_neuron = 0;
+  for(size_t i = 0; i < network->nb_layer; i++)
+  {
+    nb_total_neuron = nb_total_neuron + network->nb_neuron_layer[i];
+  }
+  savefile("NeuralData/input.txt", network->input, nb_total_neuron);
+  int size_weight_list = 0;
+  for(size_t i = 1; i < network->nb_layer; i++)
+  {
+    size_weight_list = size_weight_list + (network->nb_neuron_layer[i-1] *
+                                            network->nb_neuron_layer[i]);
+  }//calculate the size of the weight list
+  savefile("NeuralData/weights.txt", network->weights, size_weight_list);
+  savefile("NeuralData/biasWeight.txt", network->biasWeights,
+          nb_total_neuron-network->nb_neuron_layer[0]);
 }
--- AI/NeuralNetwork.h
+++ AI/NeuralNetwork.h
@@ -2,8 +2,23 @@
 #define NEURALNETWORK_H
 
 #include <stddef.h>
+#include "neuron.h"
 
-void randomizeNetwork(double *tab, size_t size);
+struct Network {
+  int nb_layer;
+  int *nb_neuron_layer; //contains the numbers of neurons per layer
+  struct Neuron *neuron_list;
+  double *input;
+  int *nb_weights; //contains the numbers of weights per layer
+  double *weights;
+  double *biasWeights;
+};
+
+struct Network *init(int nb_layer, int *nb_neuron);
+char execute_network(struct Network *network, double *inputs);
+void destroy_network(struct Network *network);
+void randomizeNetwork(struct Network *network);
+void save_Network(struct Network *network);
 
 #endif
 
